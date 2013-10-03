451f76a0244791e6502fafa74d5d6fede85556df
Dave Thorpe <thorpeda@msu.edu>
2013-10-03 08:23:01 -0400

    Removing files.

--- data/xul.js
+++
@@ -1,9 +0,0 @@
-function buttonPressed(event){
-  alert('Button was pressed!');
-}
-
-var btn = document.getElementById("foo-button");
-
-btn.addEventListener('command', buttonPressed, true);
-console.log("merp derp");
-alert("fak");
