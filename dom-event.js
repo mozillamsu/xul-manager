4a05345319bea4c0dc2ebfd00d6f723de86326e5
Eric Slenk <slenkeri@msu.edu>
2013-10-29 19:58:48 -0400

    Commented the DOMEvent class.

--- dom-event.js
+++ dom-event.js
@@ -1,3 +1,11 @@
+/**
+ * Class which maintains an event and its associated element and callback function.
+ *
+ * @constructor
+ * @param {string} elementId ID of the element to which the event is registered.
+ * @param {string} eventName Name of the event for which we are listening.
+ * @param {function} func Callback function to be executed when the event fires.
+ */
 function DOMEvent(elementId, eventName, func) {
     return {
         ElementId: elementId,
